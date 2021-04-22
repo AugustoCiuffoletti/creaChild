a7c49470f3f9e1286a8a581f3a702ff316a30ab0
AugustoCiuffoletti <augusto@di.unipi.it>
2021-04-22 09:43:43 +0200

    problema export

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { OpenweatherService } from './openweather.service';
 
-class cityTemp {
+export class cityTemp {
   luogo: string;
   valore: string
 }
