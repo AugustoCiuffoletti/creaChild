16d27a09498734cbe1b51dd6b6ccf4759914817d
AugustoCiuffoletti <augusto@di.unipi.it>
2021-04-22 14:57:35 +0200

    1 file modified

--- src/app/notifica/notifica.component.ts
+++ src/app/notifica/notifica.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { cityTemp } from '../app.component'
+import { cityTemp } from '../app.component';
 
 @Component({
   selector: 'app-notifica',
